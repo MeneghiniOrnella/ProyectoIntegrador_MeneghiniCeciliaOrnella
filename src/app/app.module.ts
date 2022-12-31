89e77ab65fd2cba48362bb126cd36d93d996d5e4
Ornella Meneghini <meneghini.ornella@gmail.com>
2022-12-31 00:22:11 -0300

    add circle-progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -11,6 +12,9 @@ import { AboutComponent } from './components/about/about.component';
 import { ExperienceComponent } from './components/experience/experience.component';
 import { EducationComponent } from './components/education/education.component';
 
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HysComponent } from './components/hys/hys.component';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,11 +24,15 @@ import { EducationComponent } from './components/education/education.component';
     BannerComponent,
     AboutComponent,
     ExperienceComponent,
-    EducationComponent
+    EducationComponent,
+    HysComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({
+      
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
